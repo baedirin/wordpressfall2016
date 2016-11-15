edbdd22204285846defe638afcad678583ef5cb3
bdavis11 <bdavis11@seattlecentral.edu>
2016-11-14 17:08:53 -0800

    updates on function.php and sidebar.php

---
+++ function.php
@@ -0,0 +1,40 @@
+<?php
+
+register_sidebar(array(
+  'before_widget' => '<div id= "%1$s" class= "widget %2$s">',
+  'after_widget' => '</div>',
+  'before_title' => '<h2>',
+  'after_title' => '</h2>',
+
+));
+
+register_nav_menus (array(
+  'main-menu' => __('Main'),
+));
+
+add_theme_support('post-thumbnails');
+
+add_post_type_support('page', 'excerpt');
+
+function get_my_title_tag() {
+
+  global $post;
+  if (is_front_page()) {
+    bloginfo('description');
+  }
+  elseif (is_page()) || is_single()) {
+    the_title();
+  }
+  else {
+    bloginfo('description');
+  }
+  if ($post->post_parent){
+    echo ' | ';
+    echo get_the_title($post->post_parent);
+  }
+  echo ' | ';
+  bloginfo('name');
+  echo ' | ';
+  echo 'Seattle, WA.';
+}
+?>
---
+++ sidebar.php
@@ -0,0 +1,25 @@
+<div id= "sidebar-primary" class= "sidebar">
+  <div id= "sub-navigation" class= "widget">
+  <?php if (is_page()) : ?>
+  <h2 class= "sub-navigation-title">
+  <?php echo get_the_title($post->post_parent);?></h2>
+  <ul class= "sub-navigation-items"><?php
+    if ($post->postparent) {
+      wp_list_pages(array('child_of' => $post->post_parent, 'title_li' => __('')));
+    } else {
+      wp_list_pages(array('child_of' => $post->ID, 'title_li' => __('')));
+    }
+   ?></ul>
+
+ <?php endif; ?>
+ <?php if(!(is_page())) : ?>
+   <h2 class= "sub-navigation-title">Blog</h2>
+   <ul class= "sub-navigation-items"><?php wp_list_categories(array('title_li' => __(''))); ?></ul>
+ <?php endif; ?>
+</div>
+
+<?php if (get_post_meta($post->ID, 'Quote', true)) : ?>
+<blockquote><?php echo get_post_meta($post->ID, 'Quote', true); ?></blockquote>
+<?php endif; ?>
+
+<?php dynamic_sidebar(1); ?>
